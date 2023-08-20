e3499b0260d1ee237a3487039ea8ae82a7a849fd
Subhrajit Bhowmik <subhrajitbhowmik80@gmail.com>
2023-08-20 15:30:09 +0530

    made collapsible sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,12 +1,28 @@
 // Sidebar.tsx
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 
 const Sidebar: React.FC = () => {
   const location = useLocation();
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
 
   return (
-    <div className="text-center bg-blue-500 h-screen w-20 fixed top-0 left-0 p-2">
+    <div className={`bg-blue-500 h-screen w-20 fixed top-0 left-0 p-2 transition-transform duration-300 ${isSidebarOpen ? 'transform translate-x-0' : '-translate-x-full'}`}>
+      <div className="flex justify-between items-center">
+        <button
+          className="text-white text-xl focus:outline-none"
+          onClick={toggleSidebar}
+        >
+          {isSidebarOpen ? '←' : '☰'}
+        </button>
+        <Link to="/" className="text-white text-xl font-bold">
+          My App
+        </Link>
+      </div>
       <ul className="flex flex-col items-center mt-8 space-y-4">
         <li>
           <Link
